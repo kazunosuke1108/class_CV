7a6aefa2af1e8141a89c879ea82dfe7bdadce352
kazunosuke <hyperturbo.kazunosuke@gmail.com>
2022-04-25 18:37:07 +0900

    done

--- exercise0425/ex0425.py
+++ exercise0425/ex0425.py
@@ -7,7 +7,7 @@ height, width, _ = img.shape
 cv2.imshow('input', img)
 cv2.waitKey(0)
 
-pts1 = np.float32([[176,187],[465,94],[98, 411],[572,356]])
+pts1 = np.float32([[343,385],[507,336],[347, 616],[527,626]])
 pts2 = np.float32([[100,200],[500,200],[100,700],[500, 700]])
 M = cv2.getPerspectiveTransform(pts1,pts2)
 dst = cv2.warpPerspective(img, M, (600, 800))
