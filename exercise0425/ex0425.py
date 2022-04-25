33a436dabdc475e48c89070fcee331dcdc856cff
kazunosuke <hyperturbo.kazunosuke@gmail.com>
2022-04-25 18:56:29 +0900

    end maybe?

--- exercise0425/ex0425.py
+++ exercise0425/ex0425.py
@@ -3,14 +3,17 @@ import cv2
 
 img=cv2.imread("exercise0425/IMG_7678.jpg")
 height, width, _ = img.shape
-
+"""
+for point in [[1560,1728],[2304,1500],[1616, 2760],[2400,2808]]:
+    cv2.circle(img, tuple(point), 10, (255,255,255), thickness=1, lineType=cv2.LINE_8, shift=0)
+"""
 cv2.imshow('input', img)
 cv2.waitKey(0)
 
-pts1 = np.float32([[343,385],[507,336],[347, 616],[527,626]])
-pts2 = np.float32([[100,200],[500,200],[100,700],[500, 700]])
+pts1 = np.float32([[1560,1728],[2304,1500],[1616, 2760],[2400,2808]])
+pts2 = np.float32([[500,500],[2500,500],[500,3500],[2500,3500]])
 M = cv2.getPerspectiveTransform(pts1,pts2)
-dst = cv2.warpPerspective(img, M, (600, 800))
+dst = cv2.warpPerspective(img, M, (3000, 4000))
 
 cv2.imshow('output',dst)
 cv2.waitKey(0)
