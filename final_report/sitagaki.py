43e6d7a6aae92c89e992e4417d50db477b288e4b
kazunosuke <hyperturbo.kazunosuke@gmail.com>
2022-06-20 19:17:56 +0900

    face recogまで

---
+++ final_report/sitagaki.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+face_cascade_path = '/Users/hayashidekazuyuki/opt/anaconda3/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
+
+#captureの準備
+cap = cv2.VideoCapture(0)
+
+def face_extract(frame):
+    face_recog=False
+    src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(src_gray)
+    for j, (x, y, w, h) in enumerate(faces):
+        if w>100 and h>100:
+            print(x,y)
+            face=frame[y:y+h,x:x+w]
+            return x,y,w,h,face
+    return 0,0,0,0,np.zeros([400,400])
+#起動と画面表示まで
+while(1):
+    #capture frameの作成
+    _, frame = cap.read()
+    cv2.imshow('Original', frame)
+    _,_,_,_,face=face_extract(frame)
+    cv2.imshow('face',face)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
