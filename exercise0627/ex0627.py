8c831e7796ef1648b487992c926efcb8e0588c34
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-06-28 15:27:18 +0900

    done

--- exercise0627/ex0627.py
+++ exercise0627/ex0627.py
@@ -7,6 +7,7 @@ import os
 from camera import camera
 from mpl_toolkits.mplot3d import Axes3D
 
+
 def match_feature(img1, img2):
     # SIFTを用意
     sift = cv2.SIFT_create()
@@ -27,7 +28,7 @@ def match_feature(img1, img2):
     img2_pt = [list(map(int, kp2[m.trainIdx].pt))
                for m in good]  # マッチした２枚目の特徴点
     img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good2,
-                             None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)  # マッチ状況の描画
+                              None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)  # マッチ状況の描画
     plt.imshow(img3), plt.show()  # 確認のためマッチの様子を出力
 
     img1_pt_s = []  # 重複削除後のマッチした特徴点
@@ -41,15 +42,16 @@ def match_feature(img1, img2):
     img2_pt_s = np.array(img2_pt_s)
     return img1_pt_s, img2_pt_s
 
+
 os.chdir("class_CV/exercise0627")
-current_dir=os.getcwd()
+current_dir = os.getcwd()
 # camera(current_dir+"/images",0)
 # img1=cv2.imread(current_dir+"/images/img1.jpg",cv2.IMREAD_GRAYSCALE)
 # img2=cv2.imread(current_dir+"/images/img2.jpg",cv2.IMREAD_GRAYSCALE)
 # img1=cv2.imread(current_dir+"/einstein/einstein1.jpg",cv2.IMREAD_GRAYSCALE)
 # img2=cv2.imread(current_dir+"/einstein/einstein2.jpg",cv2.IMREAD_GRAYSCALE)
-img1=cv2.imread(current_dir+"/sosokan/IMG_8573.jpg",cv2.IMREAD_GRAYSCALE)
-img2=cv2.imread(current_dir+"/sosokan/IMG_8574.jpg",cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread(current_dir+"/sosokan/IMG_8573.jpg", cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread(current_dir+"/sosokan/IMG_8574.jpg", cv2.IMREAD_GRAYSCALE)
 # img1=cv2.imread(current_dir+"/fukinuke/IMG_8577.jpg",cv2.IMREAD_GRAYSCALE)
 # img2=cv2.imread(current_dir+"/fukinuke/IMG_8579.jpg",cv2.IMREAD_GRAYSCALE)
 # img1=cv2.imread(current_dir+"/tana/IMG_8592.jpg",cv2.IMREAD_GRAYSCALE)
@@ -59,55 +61,61 @@ img2=cv2.imread(current_dir+"/sosokan/IMG_8574.jpg",cv2.IMREAD_GRAYSCALE)
 
 img1_pt_s, img2_pt_s = match_feature(img1, img2)
 F, mask = cv2.findFundamentalMat(img1_pt_s, img2_pt_s, cv2.RANSAC, 3, 0.99)
-print("F matrix\n",F)
+print("F matrix\n", F)
 
 # K=np.array([[842.50011162,0.,578.89029916],[0.,801.01078582,246.00138272],[0.,0.,1.]]) # 広角カメラ
-K=np.array([[2.93374279e+03, 0.00000000e+00, 1.96523556e+03],[0.00000000e+00, 2.94658602e+03, 1.43472275e+03],[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]) # 通常カメラ
-E = K.T.dot(F).dot(K)# essential matrix
-print("K matrix\n",K)
+K = np.array([[2.93374279e+03, 0.00000000e+00, 1.96523556e+03], [0.00000000e+00, 2.94658602e+03,
+             1.43472275e+03], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])  # 通常カメラ
+E = K.T.dot(F).dot(K)  # essential matrix
+print("K matrix\n", K)
 
-retval, H1, H2 = cv2.stereoRectifyUncalibrated(img1_pt_s, img2_pt_s, F, img1.shape[:2])# H matrix for rectification
-print("H1 matrix\n",H1)
-print("H2 matrix\n",H2)
-img1r = cv2.warpPerspective(img1, H1, [img1.shape[1],img1.shape[0]])# rectify the original image
-img2r = cv2.warpPerspective(img2, H2, [img2.shape[1],img2.shape[0]])
-cv2.imwrite(current_dir+"/results/img1r.jpg",img1r)
-cv2.imwrite(current_dir+"/results/img2r.jpg",img2r)
+retval, H1, H2 = cv2.stereoRectifyUncalibrated(
+    img1_pt_s, img2_pt_s, F, img1.shape[:2])  # H matrix for rectification
+print("H1 matrix\n", H1)
+print("H2 matrix\n", H2)
+# rectify the original image
+img1r = cv2.warpPerspective(img1, H1, [img1.shape[1], img1.shape[0]])
+img2r = cv2.warpPerspective(img2, H2, [img2.shape[1], img2.shape[0]])
+cv2.imwrite(current_dir+"/results/img1r.jpg", img1r)
+cv2.imwrite(current_dir+"/results/img2r.jpg", img2r)
 
-stereo = cv2.StereoSGBM_create(numDisparities=32, blockSize=9)# インスタンス化
-disparity = stereo.compute(img1r,img2r)# rectificationしたあとの画像から、disparity画像を生成
-cv2.imwrite(current_dir+"/results/img_disp_raw.jpg", disparity)
-print(disparity.max(),disparity.min())
-disparity=(disparity-disparity.min())/(disparity.max()-disparity.min())*256 # 正規化（0から255へ）
-kernel=np.ones((10,10),np.float32)/100# ノイズ除去のため平均化フィルタ実装
-disparity=cv2.filter2D(disparity,-1,kernel)
-cv2.GaussianBlur(disparity,(299,299),0)# ノイズ除去のためガウシアンフィルタ実装
-kernel=np.ones((10,10),np.float32)/100# ノイズ除去のため平均化フィルタ実装
-disparity=cv2.filter2D(disparity,-1,kernel)
-cv2.GaussianBlur(disparity,(299,299),0)# ノイズ除去のためガウシアンフィルタ実装
+# セミグローバルブロックマッチングアルゴリズムによるインスタンス化
+stereo = cv2.StereoSGBM_create(numDisparities=32, blockSize=9)
+# rectificationしたあとの画像から、disparity画像を生成
+disparity_raw = stereo.compute(img1r, img2r)
+cv2.imwrite(current_dir+"/results/img_disp_raw.jpg", disparity_raw)
+disparity = (disparity_raw-disparity_raw.min()) / \
+    (disparity_raw.max()-disparity_raw.min())*256  # 正規化（0から255へ）
+kernel = np.ones((10, 10), np.float32)/100  # ノイズ除去のため平均化フィルタ実装
+disparity = cv2.filter2D(disparity, -1, kernel)
+cv2.GaussianBlur(disparity, (299, 299), 0)  # ノイズ除去のためガウシアンフィルタ実装
+kernel = np.ones((10, 10), np.float32)/100  # ノイズ除去のため平均化フィルタ実装
+disparity = cv2.filter2D(disparity, -1, kernel)
+cv2.GaussianBlur(disparity, (299, 299), 0)  # ノイズ除去のためガウシアンフィルタ実装
 cv2.imwrite(current_dir+"/results/img_disp.jpg", disparity)
 
-def interpolate(i, imgL, imgR, disparity):
-    img_interp = np.zeros_like(imgL)
-    for y in range(imgL.shape[0]):
-        for x1 in range(imgL.shape[1]):
-            if y%10==0 and x1%10==0:
-                print("current process: ",y,x1)
-            x2 = int(x1 - disparity[y, x1])
-            x_i = int((2 - i) * x1 + (i - 1) * x2)
+
+def interpolate(i, imgL, imgR, disparity_raw):
+    img_interp = np.zeros_like(imgL)  # 描画のキャンバスを用意
+    for y in range(imgL.shape[0]):  # 画像の縦方向
+        for x1 in range(imgL.shape[1]):  # 画像の横方向
+            if y % 100 == 0 and x1 % 100 == 0:
+                print("current process: ", y, x1)
+            x2 = int(x1 - disparity[y, x1])  # 左側画像に対応する点の右側座標を取得
+            x_i = int((2 - i) * x1 + (i - 1) * x2)  # 左側画像と右側座標の間の位置を決定
+            # 移動先が画像のサイズを超えていないか確認
             if 0 <= x_i < img_interp.shape[1] and 0 <= x2 < imgR.shape[1]:
-                img_interp[y, x_i] = int((2 - i) * imgL[y, x1] + (i - 1) * imgR[y, x2])
+                img_interp[y, x_i] = int(
+                    (2 - i) * imgL[y, x1] + (i - 1) * imgR[y, x2])  # 　移動先の画素値を決定
     return img_interp.astype(np.uint8)
 
-img_interp = interpolate(1.5, img1r, img2r, disparity)
-cv2.imwrite(current_dir+"/results/view_interpolation/img_interp.jpg",img_interp)
 
-
-
-
-
-
-
+for i in range(1, 10):
+    deg = 1+i/10  # 左を１，右を２としたときのバーチャルカメラの位置
+    # view interpolationを実行
+    img_interp = interpolate(deg, img1r, img2r, disparity_raw)
+    cv2.imwrite(
+        current_dir+f"/results/view_interpolation/img_interp{deg}.jpg", img_interp)
 
 
 # import disparity_interpolation
--- exercise0627/memo.py
+++ exercise0627/memo.py
@@ -1,7 +1,2 @@
-import cv2
-import numpy as np
-img=cv2.imread("/home/ytpc2019a/code_ws/class_CV/exercise0627/results/img_disp_raw.jpg")
-for i in range(10):
-    print(i)
-    img = cv2.bilateralFilter(img,30,20,20)
-cv2.imwrite("/home/ytpc2019a/code_ws/class_CV/exercise0627/img_processing/bilateral.jpg",img)
+for i in range (1,10):
+    print(1+i/10)
